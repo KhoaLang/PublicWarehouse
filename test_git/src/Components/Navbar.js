53248cb42ecf083210cc9282cb5904ae38003db4
KL4HD <tkhoa882@gmail.com>
2021-04-20 19:15:04 +0700

    enter

---
+++ test_git/src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import {Link} from 'react-route-dom'
+
+const Navbar = () => {
+    return (
+        <div className = "nav-bar"> 
+            <h1>Goom</h1>
+            <ul className="nav-item">
+                <li>Home</li>
+                <li>About</li>
+                <li>Sign In/Sign Up</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
